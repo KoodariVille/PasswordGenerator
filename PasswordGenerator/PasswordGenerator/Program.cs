71acb4f63102b99a40c027d71891d6678e1e8ca8
Ville <vill3ni3mi@gmail.com>
2018-08-04 12:49:10 +0300

    Added RandomPassword method

--- PasswordGenerator/PasswordGenerator/Program.cs
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -32,11 +32,33 @@ namespace PasswordGenerator
         //method for printing passwords
         static void PasswordPrint(int num)
         {
-            //loop for printing password given amount
-            for(int i = 0; i < num; i++)
-            {
+            int length; //password length
 
+            Write("Give password length: ");
+            bool input = int.TryParse(ReadLine(), out length);
+
+            if (input)
+            {
+                //loop for printing password given amount
+                for (int i = 0; i < num; i++)
+                {
+                    WriteLine(RandomPassword(length));
+                }
             }
+            else
+            {
+                //if input isn´t suitable respond is given
+                WriteLine("Wrong type of input.");
+            }
+        }
+
+        static string RandomPassword(int length)
+        {
+            Random random = new Random();
+            const string pool = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var chars = Enumerable.Range(0, length)
+                .Select(x => pool[random.Next(0, pool.Length)]);
+            return new string(chars.ToArray());
         }
     }
 }
